192f1d5e824e8b90d1c5206c64b3c209d5d857a0
Miriam-Nav <marnavvid2@alu.edu.gva.es>
2025-12-11 15:38:50 +0100

    Ajustes Validaciones Colonia

--- src/colonias/colonia.service.ts
+++ src/colonias/colonia.service.ts
@@ -35,30 +35,6 @@ export class ColoniaService {
 
     async createColonia(createColoniaDto: CreateColoniaDto): Promise<Colonia> {
 
-        if (typeof createColoniaDto.localizacion !== 'string') {
-            throw new HttpException('La localización debe ser texto', HttpStatus.BAD_REQUEST);
-        }
-
-        if (typeof createColoniaDto.conteo_gatos !== 'number') {
-            throw new HttpException('El conteo de gatos debe ser un número', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (typeof createColoniaDto.cantidad_comida !== 'number') {
-            throw new HttpException('La cantidad de comida debe ser un número', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (typeof createColoniaDto.horario_alimento !== 'string' && !(createColoniaDto.horario_alimento instanceof Date)) {
-            throw new HttpException('El horario de alimento debe ser una fecha válida', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (createColoniaDto.foto && typeof createColoniaDto.foto !== 'string') {
-            throw new HttpException('La foto debe ser texto (ruta de archivo)', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (typeof createColoniaDto.protectora !== 'number') {
-            throw new HttpException('El ID de la protectora debe ser un número', HttpStatus.BAD_REQUEST);
-        }
-
         // Validar protectora
         const protectora = await this.protectoraRepository.findOneBy({ id_protectora: createColoniaDto.protectora });
         if (!protectora) {
@@ -81,32 +57,6 @@ export class ColoniaService {
 
         if (!colonia) {
             throw new HttpException('Colonia no encontrada', HttpStatus.NOT_FOUND);
-        }
-
-        if (updateColoniaDto.localizacion !== undefined && typeof updateColoniaDto.localizacion !== 'string') {
-            throw new HttpException('La localización debe ser texto', HttpStatus.BAD_REQUEST);
-        }
-
-        if (updateColoniaDto.conteo_gatos !== undefined && typeof updateColoniaDto.conteo_gatos !== 'number') {
-            throw new HttpException('El conteo de gatos debe ser un número', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (updateColoniaDto.cantidad_comida !== undefined && typeof updateColoniaDto.cantidad_comida !== 'number') {
-            throw new HttpException('La cantidad de comida debe ser un número', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (updateColoniaDto.horario_alimento !== undefined &&
-            typeof updateColoniaDto.horario_alimento !== 'string' &&
-            !(updateColoniaDto.horario_alimento instanceof Date)) {
-            throw new HttpException('El horario de alimento debe ser una fecha válida', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (updateColoniaDto.foto !== undefined && typeof updateColoniaDto.foto !== 'string') {
-            throw new HttpException('La foto debe ser texto (nombre de archivo)', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (updateColoniaDto.protectora !== undefined && typeof updateColoniaDto.protectora !== 'number') {
-            throw new HttpException('El ID de la protectora debe ser un número', HttpStatus.BAD_REQUEST);
         }
 
         // Actualizar protectora si se proporciona
