1284178fa92eda8615071d834f1ac4c355a7bb22
Miriam-Nav <marnavvid2@alu.edu.gva.es>
2025-12-11 15:41:15 +0100

    Ajustes Entidad

--- src/entidad/entidad.service.ts
+++ src/entidad/entidad.service.ts
@@ -27,12 +27,7 @@ export class EntidadService {
     }
 
     async createEntidad(createEntidadDto: CreateEntidadDto): Promise<Entidad> {
-        if (typeof createEntidadDto.nombre !== 'string') {
-            throw new HttpException('El nombre debe ser texto', HttpStatus.BAD_REQUEST);
-        }
-        if (typeof createEntidadDto.tipo !== 'string') {
-            throw new HttpException('El tipo debe ser texto', HttpStatus.BAD_REQUEST);
-        }
+
         const entidad = this.entidadRepository.create(createEntidadDto);
         return this.entidadRepository.save(entidad);
     }
@@ -44,14 +39,6 @@ export class EntidadService {
 
         if (!entidad) {
             throw new HttpException('Entidad no encontrada', HttpStatus.NOT_FOUND);
-        }
-
-        if (updateEntidadDto.nombre !== undefined && typeof updateEntidadDto.nombre !== 'string') {
-            throw new HttpException('El nombre debe ser texto', HttpStatus.BAD_REQUEST);
-        }
-        
-        if (updateEntidadDto.tipo !== undefined && typeof updateEntidadDto.tipo !== 'string') {
-            throw new HttpException('El tipo debe ser texto', HttpStatus.BAD_REQUEST);
         }
 
         // Actualiza solo los campos que vienen en el DTO
